f47f49df0dbe63e2334c841f4446226838e86d20
Benefrancis do Nascimento <benefrancis@gmail.com>
2022-06-16 17:29:44 -0300

    Criando o projeto back end (Spring Boot)

---
+++ backend/src/main/java/br/com/benefrancis/fabrica/chocolate/FabricaChocolateApplication.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.fabrica.chocolate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricaChocolateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricaChocolateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/benefrancis/fabrica/chocolate/FabricaChocolateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.benefrancis.fabrica.chocolate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricaChocolateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
